35c89265190d9c3dc2502711a800bdc01b4f223b
Tanvir-Shuvo <tanvirkouser.shuvo@gmail.com>
2021-09-14 21:28:10 +0600

    initial commit

---
+++ 02--creating-object-class/dailyroutine.js
@@ -0,0 +1,52 @@
+/**
+ * Creating classes:
+ *
+ * Class declaration: class Name {}
+ * Class expression:  const Name = class {}
+ */
+class DailyRoutine {
+  constructor(
+    // Defines parameters:
+    rise,
+    shower,
+    breakfast,
+    emailCheck,
+    workStart,
+    projectA,
+    projectB,
+    lunchTime,
+    workDone,
+    refreshment,
+    dinner,
+    familyTime,
+    sleep,
+    startday
+  ) {
+    // Define properties:
+    this.rise =      
+    rise;
+    this.shower = shower;
+    this.breakfast = breakfast;
+    this.emailCheck = emailCheck;
+    this.workStart = workStart;
+    this.projects = {
+      morning: projectA,
+      afternoon: projectB,
+    };
+    this.lunchTime = lunchTime;
+    this.workDone = workDone;
+    this.refreshment = refreshment;
+    this.dinner = dinner;
+    this.familyTime = familyTime;
+    this.sleep = sleep;
+    this.startday = startday;
+  }
+  // Add methods like normal functions:
+  projectChange(morningNew, afternoonNew) {
+    this.projects.morning = morningNew;
+    this.projects.afternoon = afternoonNew;
+  }
+  // date global object
+}
+
+export default DailyRoutine;
